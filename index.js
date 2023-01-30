b204f7e52b1952cec6d4a3c03e7a9db1a7557f41
david-roa <30906467+david-roa@users.noreply.github.com>
2023-01-29 19:11:51 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-// run `node index.js` in the terminal
+// this copy -> `node index.js` in the terminal
 
-let listaAsignacion = []
+function getLogicRandom() {
+  
+  let listaAsignacion = []
 
-//aqui se modificapara aplicar un margen de error;
-let countA = 41;
-let countB = 41;
-const numeroParticipantes = 82;
-
-for (let i = 0; i < numeroParticipantes; i++){
-  listaAsignacion.push({id: i+1, assig: 'NN'});
+  //aqui se modificapara aplicar un margen de error;
+  let countA = 41;
+  let countB = 41;
+  const numeroParticipantes = 82;
+  
+  //se llenan todos los datos iniciales
+  for (let i = 0; i < numeroParticipantes; i++){
+    listaAsignacion.push({id: i+1, assig: 'NN'});
+  }
+  
+  //logica de Asignacion 
+  for (let i = 0; i < listaAsignacion.length; i++){
+    const op = getRandomInt(2);
+    if (op === 0 && countA !== 0){
+      listaAsignacion[i].assig = 'A';
+      countA = countA -1;
+    } else {
+      listaAsignacion[i].assig = 'B';
+      countB = countB -1;
+    }
+  }
+  
+  const totalA = listaAsignacion.filter(data => data.assig === 'A');
+  const totalB = listaAsignacion.filter(data => data.assig === 'B');
+  return {list: listaAsignacion, a: totalA, b: totalB}
 }
-
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-for (let i = 0; i < listaAsignacion.length; i++){
-  const op = getRandomInt(2);
-  if (op === 0 && countA !== 0){
-    listaAsignacion[i].assig = 'A';
-    countA = countA -1;
-  } else {
-    listaAsignacion[i].assig = 'B';
-    countB = countB -1;
-  }
+let result = {list: [], a: 0, b: 0};
+while (result.a !== 41 && result.b !== 41){
+  result = getLogicRandom();
 }
 
-const totalA = listaAsignacion.filter(data => data.assig === 'A');
-const totalB = listaAsignacion.filter(data => data.assig === 'B');
+
+//Tarea hay un bug, ojo
+/*
+  ~/projects/node-haut8i
+  ❯ node index
+  Total A:  39
+  Total B:  39
+*/
 
 
-console.log(listaAsignacion.filter(data => data.assig === 'A'));
+
+console.log('Total A: ', result.a);
+console.log('Total B: ', result.b);
+console.log(result.list.map(d => `${d.assig}\n\r`));
